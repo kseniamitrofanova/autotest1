2ad5f46b755d678caaef843c9d5f6b25369a7e83
KMitrofanova <vip.ksundra@mail.ru>
2023-10-30 20:28:56 +0300

    updated TextBoxTests for homework

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -3,9 +3,10 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class TextBoxTests {
 
@@ -23,18 +24,39 @@ public class TextBoxTests {
     @Test
 
     void fillFormTest() {
-        open("/text-box");
-        $("#userName").setValue("Ksenia");
-        $("#userEmail").setValue("Ksenia@mail.ru");
-        $("#currentAddress").setValue("Street 19");
-        $("#permanentAddress").setValue("Another Streen 19");
-        $("#submit").click();
+        open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove");
+        $("#firstName").setValue("Ksenia");
+        $("#lastName").setValue("Mitrofanova");
+        $("#userEmail").setValue("KMitrofanova@mail.ru");
+        $("#userNumber").setValue("8999123456");
+        $("[for='gender-radio-2']").click();
+        //$("#subjectsContainer").setValue("Russia");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("June");
+        $(".react-datepicker__year-select").selectOption("1997");
+        $(".react-datepicker__day--003").click();
+        $("#subjectsInput").val("Computer Science").pressEnter();
+        $("[for='hobbies-checkbox-1']").click();
+        $("[for='hobbies-checkbox-2']").click();
+        $("[for='hobbies-checkbox-3']").click();
+        $("#uploadPicture").uploadFromClasspath("QA.jpeg");
+        $("#currentAddress").setValue("Penza, Central street 132");
+        $("#react-select-3-input").val("NCR").pressEnter();
+        $("#react-select-4-input").val("Noida").pressEnter();
+        $("#submit").pressEnter();
 
-       $("#output").$("#name").shouldHave(text("Ksenia"));
-       $("#output").$("#email").shouldHave(text("Ksenia@mail.ru"));
-       $("#output").$("#currentAddress").shouldHave(text("Street 19"));
-       $("#output").$("#permanentAddress").shouldHave(text("Another Streen 19"));
-
+        $(".table-responsive").shouldHave(text("Ksenia Mitrofanova"));
+        $(".table-responsive").shouldHave(text("KMitrofanova@mail.ru"));
+        $(".table-responsive").shouldHave(text("Female"));
+        $(".table-responsive").shouldHave(text("8999123456"));
+        $(".table-responsive").shouldHave(text("3 June,1997"));
+        $(".table-responsive").shouldHave(text("Computer Science"));
+        $(".table-responsive").shouldHave(text("Sports, Reading, Music"));
+        $(".table-responsive").shouldHave(text("QA.jpeg"));
+        $(".table-responsive").shouldHave(text("Penza, Central street 132"));
+        $(".table-responsive").shouldHave(text("NCR Noida"));
     }
 
 }
